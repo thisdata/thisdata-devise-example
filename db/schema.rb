d1209c9fcf1d33720f048c0306ea68540834b5cb
Nick Malcolm <nick@malcolm.net.nz>
2016-04-26 17:50:12 +1200

    basic install

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150101170206) do
+ActiveRecord::Schema.define(version: 20140224203311) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
